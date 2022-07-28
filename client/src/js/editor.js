5db9e1d014cf1b7f78467ffc8307e180793d14e2
shingchan95 <chanhei888@gmail.com>
2022-07-28 18:30:30 +0100

    update on database

--- client/src/js/editor.js
+++ client/src/js/editor.js
@@ -25,6 +25,7 @@ export default class {
     // When the editor is ready, set the value to whatever is stored in indexeddb.
     // Fall back to localStorage if nothing is stored in indexeddb, and if neither is available, set the value to header.
     getDb().then((data) => {
+      console.log(data)
       console.info('Loaded data from IndexedDB, injecting into editor');
       this.editor.setValue(data || localData || header);
     });
